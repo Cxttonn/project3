115e82616c22ec2876dc7bfd67c4417df8bcaede
Cxttonn <phitchayapha_b@cmu.ac.th>
2024-08-28 11:27:48 +0700

    Add tailwind

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -1,44 +1,47 @@
-import axios from 'axios'
-
+import axios from 'axios';
+ 
 const apiClient = axios.create({
   withCredentials: false,
   headers: {
-    Accept: 'application/json'
-  }
-})
-
+    Accept: 'application/json',
+  },
+});
+ 
 export default {
   async getAllEvents() {
-    const parts = ['countrymedal/data1', 'data2/data2', 'data3/data3', 'data4/data4', 'data5/data5']
-
-    const dataPromises = parts.map((part) =>
-      apiClient
-        .get(`https://my-json-server.typicode.com/Jasmxnej/${part}`)
-        .then((response) => response.data)
-    )
-    const results = await Promise.all(dataPromises)
-
-    const allEvents = results.flat()
-    return allEvents
+    const parts = [
+      'countrymedal/data1',
+      'data2/data2',
+      'data3/data3',
+      'data4/data4',
+      'data5/data5',
+    ];
+ 
+    const dataPromises = parts.map(part =>
+      apiClient.get(`https://my-json-server.typicode.com/Jasmxnej/${part}`).then(response => response.data)
+    );
+    const results = await Promise.all(dataPromises);
+ 
+    const allEvents = results.flat();
+    return allEvents;
   },
-
+ 
   async getEvent(id: string) {
-    const events = await this.getAllEvents()
-    const event = events.find((event) => event.id === id)
+    const events = await this.getAllEvents();
+    const event = events.find(event => event.id === id);
     if (!event) {
-      throw new Error(`Event with ID ${id} not found`)
+      throw new Error(`Event with ID ${id} not found`);
     }
-    return event
+    return event;
   },
-
+ 
   async getCountryDetails(id: string) {
-    const response = await apiClient.get(
-      `https://my-json-server.typicode.com/matchimaky/dbolympic/country`
-    )
-    const country = response.data.find((country: any) => country.id === id)
+    const response = await apiClient.get(`https://my-json-server.typicode.com/matchimaky/dbolympic/country`);
+    const country = response.data.find((country: any) => country.id === id);
     if (!country) {
-      throw new Error(`Country with ID ${id} not found`)
+      throw new Error(`Country with ID ${id} not found`);
     }
-    return country
-  }
-}
+    return country;
+  },
+};
+ 
